192055c0b248d3a5793748e2ba5faa76b53466ee
Jens Haase <je.haase@googlemail.com>
2015-02-09 11:28:34 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sockjs-clojure "0.1.1-SNAPSHOT"
+(defproject sockjs-clojure "0.1.1"
   :description "A sockjs implementation on top of http-kit server"
   :url "https://github.com/jenshaase/sockjs-clojure"
   :license {:name "Eclipse Public License"
